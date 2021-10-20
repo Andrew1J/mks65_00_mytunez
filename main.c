746f7380fdc457e853beb047e56a7d5435e4b018
Andrew <andrewjuang01@gmail.com>
2021-10-20 10:17:50 -0400

    add main comments

--- main.c
+++ main.c
@@ -12,13 +12,14 @@ void library_test();
 int main() {
     linked_list_test();
     library_test();
-    
+
     return 0;
 }
 
 
 // Tests for the linked list part
 void linked_list_test() {
+    // insert_front tests
     struct song *songs = make_song("street spirit (fade out)", "radiohead");
     songs = insert_front(songs, "paranoid android", "radiohead");
     songs = insert_front(songs, "time", "pink floyd");
@@ -28,24 +29,27 @@ void linked_list_test() {
     songs = insert_front(songs, "alive", "pearl jam");
     songs = insert_front(songs, "thunderstruck", "ac/dc");
 
+    // Linked List tests
     printf("LINKED LIST TESTS\n");
     printf("====================================\n\n");
 
+    // print_list tests
     printf("Testing print_list:\n");
     print_list(songs);
 
+    // print_song tests
     printf("====================================\n\n");
     printf("Testing print_song:\n");
     print_song(songs);
 
+    // find_song tests
     printf("====================================\n\n");
     printf("Testing find_song:\n");
     printf("looking for [pearl jam: even flow]\n");
     struct song *search = find_song(songs, "even flow", "pearl jam");
     if (search) {
         printf("node found! %s: %s\n", search->artist, search->title);
-    }
-    else {
+    } else {
         printf("node not found\n");
     }
 
@@ -53,11 +57,11 @@ void linked_list_test() {
     search = find_song(songs, "daughter", "pearl jam");
     if (search) {
         printf("node found! %s: %s\n", search->artist, search->title);
-    }
-    else {
+    } else {
         printf("node not found\n");
     }
 
+    // find_artists_song tests
     printf("====================================\n\n");
     printf("Testing find_artists_song:\n");
     printf("looking for [pink floyd]\n");
@@ -90,7 +94,14 @@ void linked_list_test() {
         printf("artist not found\n");
     }
 
-    // more tests later
+    // random_song tests
+
+    // remove_song tests
+    print_list(songs);
+    printf("\n");
+    songs = remove_song(songs,"yellow ledbetter", "pearl jam");
+    print_list(songs);
+
 }
 
 
