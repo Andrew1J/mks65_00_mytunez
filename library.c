5b7e719667e79d6b01db1f257555bf4b8a462ebf
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-10-22 10:45:26 -0400

    added function bodies to library.c

--- library.c
+++ library.c
@@ -5,3 +5,39 @@ struct song ** make_library() {
     struct song ** library[27];
     return library;
 }
+
+struct song ** add_song(struct song **library) {
+    return;
+}
+
+struct song * find_song_in_library(struct song **library, char *title, char *artist) {
+    return;
+}
+
+struct song * find_artist_in_library(struct song **library, char *artist) {
+    return;
+}
+
+void print_letter(struct song **library, char letter) {
+    return;
+}
+
+void print_artist(struct song **library, char *artist) {
+    return;
+}
+
+void print_library(struct song **library) {
+    return;
+}
+
+void shuffle_library(struct song **library, int num_songs) {
+    return;
+}
+
+struct song ** delete_song(struct song **library, char *title, char *artist) {
+    return;
+}
+
+struct song ** clear_library(struct song **library) {
+    return;
+}
