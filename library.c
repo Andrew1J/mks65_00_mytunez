928ead8dad74acfd576d29483b23cf1f09757e59
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-10-23 10:55:41 -0400

    added comments for library.c functions, found some bugs as well

--- library.c
+++ library.c
@@ -6,11 +6,16 @@
 #include "songs.h"
 #include "library.h"
 
+
+// makes a new library
 struct song ** make_library(struct song **library) {
     library = calloc(27, sizeof(struct song *));
+
     return library;
 }
 
+
+// adds a song to library
 struct song ** add_song(struct song **library, char *title, char *artist) {
     int ind = conv_artist_to_index(artist);
 
@@ -19,18 +24,30 @@ struct song ** add_song(struct song **library, char *title, char *artist) {
     return library;
 }
 
+
+// finds a certain song in the library
 struct song * find_song_in_library(struct song **library, char *title, char *artist) {
     int ind = conv_artist_to_index(artist);
 
     return find_song(library[ind], title, artist);
 }
 
+
+// finds the first song of a certain artist
+// in the library
 struct song * find_artist_in_library(struct song **library, char *artist) {
     int ind = conv_artist_to_index(artist);
 
     return find_artists_song(library[ind], artist);
 }
 
+
+// prints all the songs in a certain category
+// in the library
+//
+// TODO:
+// could call print_list from songs.h
+// doesn't work for symbols
 void print_letter(struct song **library, char letter) {
     struct song * temp = library[letter - 97];
 
@@ -44,10 +61,17 @@ void print_letter(struct song **library, char letter) {
     printf("]\n");
 }
 
+
+// prints out all the songs of a certain artist
 void print_artist(struct song **library, char *artist) {
     return;
 }
 
+
+// prints the contents of the entire library
+//
+// TODO:
+// doesn't print the symbols catagory
 void print_library(struct song **library) {
     int i;
 
@@ -58,10 +82,14 @@ void print_library(struct song **library) {
     }
 }
 
+
+// returns a series of randomly chosen songs
 // void shuffle_library(struct song **library, int num_songs) {
 //     return;
 // }
 
+
+// deletes a song from the library
 struct song ** delete_song(struct song **library, char *title, char *artist) {
     int ind = conv_artist_to_index(artist);
 
@@ -70,6 +98,11 @@ struct song ** delete_song(struct song **library, char *title, char *artist) {
     return library;
 }
 
+
+// clears the entire library
+//
+// TODO:
+// doesn't clear the symbols category
 struct song ** clear_library(struct song **library) {
     int i;
 
@@ -80,6 +113,12 @@ struct song ** clear_library(struct song **library) {
     return library;
 }
 
+
+// Returns the index of the correct category
+// for a given artist
+//
+// TODO:
+// errors for non-alpha characters
 int conv_artist_to_index(char *artist) {
     return ((int) tolower(artist[0])) - 97;
 }
